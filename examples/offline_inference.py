327eba1fee7e414edef0aa2fb8cdf75ae27aab4a
liupeng <itsliupeng@gmail.com>
2024-08-23 10:47:22 +0000

    add profile

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -20,6 +20,8 @@ prompts = [
     "The breakthrough technique developed by University of Oxford researchers could one day provide tailored repairs for those who suffer brain injuries. The researchers demonstrated for the first time that neural cells can be 3D printed to mimic the architecture of the cerebral cortex. The results have been published today in the journal Nature Communications. Brain injuries, including those caused by trauma, stroke and surgery for brain tumours, typically result in significant damage to the cerebral cortex (the outer layer of the human brain), leading to difficulties in cognition, movement and communication. For example, each year, around 70 million people globally suffer from traumatic brain injury (TBI), with 5 million of these cases being severe or fatal. Currently, there are no effective treatments for severe brain injuries, leading to serious impacts on quality of life. Tissue regenerative therapies, especially those in which patients are given implants derived from their own stem cells, could be a promising route to treat brain injuries in the future. Up to now, however, there has been no method to ensure that implanted stem cells mimic the architecture of the brain.",
     "Hydrogen ions are the key component of acids, and as foodies everywhere know, the tongue senses acid as sour. That's why lemonade (rich in citric and ascorbic acids), vinegar (acetic acid) and other acidic foods impart a zing of tartness when they hit the tongue. Hydrogen ions from these acidic substances move into taste receptor cells through the OTOP1 channel. Because ammonium chloride can affect the concentration of acid -- that is, hydrogen ions -- within a cell, the team wondered if it could somehow trigger OTOP1. To answer this question, they introduced the Otop1 gene into lab-grown human cells so the cells produce the OTOP1 receptor protein. They then exposed the cells to acid or to ammonium chloride and measured the responses. We saw that ammonium chloride is a really strong activator of the OTOP1 channel, Liman said. It activates as well or better than acids. Ammonium chloride gives off small amounts of ammonia, which moves inside the cell and raises the pH, making it more alkaline, which means fewer hydrogen ions.",
 ]
+
+
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=100)
 
@@ -30,12 +32,13 @@ replica_config = ReplicaConfig(
 )
 
 model_config = ModelConfig(
-    model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    model="/cpfs/data/shared/public/models/Yi-1.5-9B-Chat",
 )
 
 parallel_config = ParallelConfig(
-    tensor_parallel_size=1,
-    pipeline_parallel_size=4,
+    tensor_parallel_size=4,
+    pipeline_parallel_size=1,
+    # pipeline_parallel_size=1,
 )
 
 scheduler_config = SarathiSchedulerConfig(
@@ -89,7 +92,8 @@ def generate(
 
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
-outputs = generate(llm_engine, prompts, sampling_params)
+for i in range(300):
+    outputs = generate(llm_engine, prompts, sampling_params)
 # Print the outputs.
 for output in outputs:
     prompt = output.prompt
